a33251e163747a755ba32a5d3141dcb259379bd4
Olof Hoverfalt <hoverfalt@gmail.com>
2020-11-28 13:32:39 +0200

    Site update

---
+++ Update_website.R
@@ -0,0 +1,10 @@
+# Threddit webstie update
+
+# Terminal commands to re-initiate git repo, add commit and push to master
+
+# git init
+# git add --all
+# git commit -m "First new commit"
+# git remote add origin https://github.com/hoverfalt/hoverfalt.github.io
+# git push -f origin master
+
