ee736be931d7c80213c4fca66e50f9ba568cc618
Olof Hoverfalt <hoverfalt@gmail.com>
2020-11-28 17:47:43 +0200

    First complete portfolio page

--- Update_website.R
+++ Update_website.R
@@ -2,8 +2,10 @@
 
 # Terminal commands to re-initiate git repo, add commit and push to master
 
+# git config --get remote.origin.url
 # git init
 # git add --all
+# git rm .DS_Store
 # git commit -m "First new commit"
 # git remote add origin https://github.com/hoverfalt/hoverfalt.github.io
 # git push -f origin master
